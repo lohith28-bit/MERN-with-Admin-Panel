5f56eeacba2c9d727e776a6d9a9065aa788ccf6f
Lohith V P <lohithvp18@gmail.com>
2023-12-04 19:44:22 +0530

    Updated front-end of about and contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,73 +1,99 @@
-import img from '../../public/images/home.png'
-import { useState } from 'react'
+import { useState } from "react";
 
 export const Contact = () => {
-	const [user, setUser] = useState({
+	const [contact, setContact] = useState({
 		username: "",
 		email: "",
-		message: ""
-	})
+		message: "",
+	});
 
+	const handleInput = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
 
-
-	const handleChange = (e) => {
-		let value = e.target.value
-		let name = e.target.name;
-		setUser({
-			...user,
-			[name]: value
-		})
-	}
+		setContact({
+			...contact,
+			[name]: value,
+		});
+	};
 
 	const handleSubmit = (e) => {
-		e.preventDefault()
-		console.log(user)
-	}
+		e.preventDefault();
+
+		console.log(contact);
+	};
+
 
 	return (
 		<>
-			<div>
-				<div>
-					<h1>Contacts</h1>
+			<section className="section-contact">
+				<div className="contact-content container">
+					<h1 className="main-heading">contact us</h1>
 				</div>
-				<div>
-					<div>
-						<img src={img} alt="photo" width="400px" />
+				{/* contact page main  */}
+				<div className="container grid grid-two-cols">
+					<div className="contact-img">
+						<img src="/images/support.png" alt="we are always ready to help" />
 					</div>
-					<div>
+
+					{/* contact form content actual  */}
+					<section className="section-form">
 						<form onSubmit={handleSubmit}>
 							<div>
-								<label>Username</label>
-								<input type="text"
-									placeholder='Enter username'
-									name='username'
-									value={user.username}
-									onChange={handleChange}
+								<label htmlFor="username">username</label>
+								<input
+									type="text"
+									name="username"
+									id="username"
+									autoComplete="off"
+									value={contact.username}
+									onChange={handleInput}
 									required
 								/>
 							</div>
+
 							<div>
-								<label>email</label>
-								<input type="email" name="email" placeholder='Enter your email'
-									value={user.email}
-									onChange={handleChange}
+								<label htmlFor="email">email</label>
+								<input
+									type="email"
+									name="email"
+									id="email"
+									autoComplete="off"
+									value={contact.email}
+									onChange={handleInput}
 									required
 								/>
 							</div>
+
 							<div>
-								<label>Message</label>
-								<textarea name="message"
-									placeholder='Please type your message' cols="30" rows="10"
-									value={user.message}
-									onChange={handleChange} />
+								<label htmlFor="message">message</label>
+								<textarea
+									name="message"
+									id="message"
+									autoComplete="off"
+									value={contact.message}
+									onChange={handleInput}
+									required
+									cols="30"
+									rows="6"
+								></textarea>
 							</div>
-						<div>
-							<button type='submit'>Submit</button>
-						</div>
+
+							<div>
+								<button type="submit">submit</button>
+							</div>
 						</form>
-					</div>
+					</section>
 				</div>
-			</div>
+
+				<section className="mb-3">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2988.5522592989023!2d77.59513639171112!3d12.899512617583559!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1517961f6b5d%3A0xbb0b209eb8161d4d!2sKalyani%20Magnum!5e0!3m2!1sen!2sin!4v1701698917234!5m2!1sen!2sin" width="100%"
+						height="450"
+						allowFullScreen
+						loading="lazy"
+						referrerPolicy="no-referrer-when-downgrade"></iframe>		
+				</section>
+			</section>
 		</>
-	)
-}
+	);
+};
